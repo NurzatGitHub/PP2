8e84d0275b73e4e6c6106dc5c3454ea6a651cb46
Nurzat <n_turganbek@kbtu.kz>
2023-01-19 14:57:01 +0600

    week1 tasks

---
+++ 1Intro.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+"""
+This is a comment
+written in
+more than just one line
+"""
