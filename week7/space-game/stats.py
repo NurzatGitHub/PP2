d23a811785ff162aa6edc6ceb7829c14ec58fc95
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-03-15 10:09:37 +0600

    week 7

---
+++ week7/space-game/stats.py
@@ -0,0 +1,11 @@
+class Stats():
+
+    def __init__(self):
+        self.reset_stats()
+        self.run_game = True
+        with open(r'C:\Users\Nurza\Documents\PP2\week7\space-game\image\high_score.txt','r') as f:
+            self.high_score = int(f.readline())
+        
+    def reset_stats(self):
+        self.guns_left = 3
+        self.score = 0
