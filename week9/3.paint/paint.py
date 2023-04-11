e86168bef168b005c117577b486b812ad52bbf51
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-04-11 13:47:27 +0600

    week 9

--- week9/3.paint/paint.py
+++ week9/3.paint/paint.py
@@ -15,6 +15,25 @@ yellow_mode = (255, 234, 0)
 radius = 15
 drawing = False
 eraser = False
+def draw_circle(pos):
+    pygame.draw.circle(screen,mode,pos,2*radius,2)
+def draw_square(pos):
+    x,y = pos
+    rect_pos = (x - radius, y - radius)
+    rect_size = (2*radius, 2*radius)
+    pygame.draw.rect(screen,mode,(rect_pos,rect_size),2)
+def draw_right_triangle(pos):
+    x,y = pos
+    pygame.draw.polygon(screen, mode,[pos, [x, y-2*radius],[x+2*radius, y]],2)
+def draw_equilateral_triangle(pos):
+    x, y = pos
+    height = int(radius * (3 ** 0.5) / 2)
+    points = [(x, y - 2*radius), (x - 2*height, y + 2*height), (x + 2*height, y + 2*height)]
+    pygame.draw.polygon(screen, mode, points, 2)
+def draw_rhombus(pos):
+    x, y = pos
+    points = [(x, y - radius), (x - radius, y), (x, y + radius), (x + radius, y)]
+    pygame.draw.polygon(screen, mode, points, 2)
 
 while True:
     clock.tick(60)
@@ -39,6 +58,21 @@ while True:
             elif event.key == pygame.K_y:
                 mode = yellow_mode
                 eraser = False
+            elif event.key == pygame.K_s:
+                draw_square(pos)
+                eraser = False
+            elif event.key == pygame.K_c:
+                draw_circle(pos)
+                eraser = False
+            elif event.key == pygame.K_p:
+                draw_right_triangle(pos)
+                eraser = False
+            elif event.key == pygame.K_t:
+                draw_equilateral_triangle(pos)
+                eraser = False
+            elif event.key == pygame.K_h:
+                draw_rhombus(pos)
+                eraser = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 pygame.draw.circle(screen, mode, pos, radius)
@@ -59,7 +93,7 @@ while True:
                 pygame.draw.circle(screen, mode, pos, radius)
         elif event.type == pygame.MOUSEWHEEL:
             if event.y > 0:
-                radius += 1
+                radius += 2
             elif event.y < 0:
-                radius -= 1
+                radius -= 2
     pygame.display.update()
