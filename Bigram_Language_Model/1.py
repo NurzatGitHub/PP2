19f99055c7b172d73dec1e49c1a8d7d5a8f3c482
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-05-04 19:52:10 +0600

    bigram

--- Bigram_Language_Model/1.py
+++ Bigram_Language_Model/1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 names = open("text.txt","r")
 
-bigram_table = {'name':[],'bigram':[]}
+bigram_table = {'name':[],'bigram':[],'bigram_freq':[]}
 
 for name in names:
     # print(name,end="")
@@ -10,12 +10,12 @@ for name in names:
     for i in range(len(name)-1):
         bigram = name[i:i+2]
         s += f"{bigram}  "
+        if bigram in bigram_table['bigram']:
+            bigram_table['bigram_freq'] += 1
+        else:
+            bigram_table['bigram_freq'] = 1
+        
     bigram_table['bigram'].append(s)
-        # if bigram in bigram_table:
-        #     bigram_table[bigram] += 1
-        # else:
-        #     bigram_table[bigram] = 1
-
 # for bigram in bigram_table:
 #     print(bigram + ": " + str(bigram_table[bigram]))
 df = pd.DataFrame(bigram_table)
