413d2e04ec3ad2dc7382b8cfbd6e99b3032dcd37
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-05-04 20:54:06 +0600

    bigram

--- Bigram_Language_Model/1.py
+++ Bigram_Language_Model/1.py
@@ -4,28 +4,31 @@ names = open("text.txt","r")
 bigram_table = {'name':[],'bigram':[],'bigram_freq':[]}
 
 all_bigrams = []
-
+# freq = 1
+num_bigrams = 0
 for name in names:
-    freq = 1
     # print(name,end="")
     bigram_table['name'].append(name)
     s = ""
     s_freq = ""
+    # num_bigrams = 0
     for i in range(len(name)-1):
         bigram = name[i:i+2]
         s += f"{bigram}  "
+        # num_bigrams += 1
         all_bigrams.append(bigram)
-        if bigram in all_bigrams:
-            freq += 1
-            # bigram_table['bigram_freq'].append(freq)
-        else:
-            freq = 1
+        freq = all_bigrams.count(bigram)
+        s_freq += f"{bigram} : {freq}   "
+    if bigram in all_bigrams:
+        freq += 1
+            # bigram_table['bigram_freq'][num_bigrams]
+            # freq = 1
+        # else:
+        #     freq = 1
             # bigram_table['bigram_freq'].append(freq)
         
-        s_freq += f"{bigram} : {freq}  "
+
     bigram_table['bigram_freq'].append(s_freq)
-
-
     bigram_table['bigram'].append(s)
 # for bigram in bigram_table:
 #     print(bigram + ": " + str(bigram_table[bigram]))
