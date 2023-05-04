eb65f889bc47ef4933afe9fdb01c314bcdd30f8b
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-05-04 21:15:47 +0600

    bigram

--- Bigram_Language_Model/main.py
+++ Bigram_Language_Model/main.py
@@ -4,35 +4,24 @@ names = open("text.txt","r")
 bigram_table = {'name':[],'bigram':[],'bigram_alternate_meeting':[]}
 
 all_bigrams = []
-# freq = 1
 num_bigrams = 0
 for name in names:
-    # print(name,end="")
     bigram_table['name'].append(name)
     s = ""
     s_freq = ""
-    # num_bigrams = 0
     for i in range(len(name)-1):
         bigram = name[i:i+2]
         s += f"{bigram}  "
-        # num_bigrams += 1
         all_bigrams.append(bigram)
         freq = all_bigrams.count(bigram)
         s_freq += f"{bigram} : {freq}   "
     if bigram in all_bigrams:
         freq += 1
-            # bigram_table['bigram_freq'][num_bigrams]
-            # freq = 1
-        # else:
-        #     freq = 1
-            # bigram_table['bigram_freq'].append(freq)
-        
 
     bigram_table['bigram_alternate_meeting'].append(s_freq)
     bigram_table['bigram'].append(s)
-# for bigram in bigram_table:
-#     print(bigram + ": " + str(bigram_table[bigram]))
+
 df = pd.DataFrame(bigram_table)
-# f = open("myfile.csv", "w")
+
 df.to_csv('information.csv',index=False)
-print(df)
+# print(df)
