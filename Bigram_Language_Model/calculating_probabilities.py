97c3416468a01add22f433856b47882d26811a0e
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-05-05 12:37:30 +0600

    bigram

--- Bigram_Language_Model/calculating_probabilities.py
+++ Bigram_Language_Model/calculating_probabilities.py
@@ -5,8 +5,8 @@ table_prob = {'quantity':[],'probability':[]}
 probability = 0
 for bigrams in set_bigrams:
     table_prob['quantity'].append(f"{bigrams}: {all_bigrams.count(bigrams)}")
-    probability = all_bigrams.count(bigrams)/len(all_bigrams)
-    table_prob['probability'].append([f"{bigrams}: {float(probability)}"])
+    probability = float(all_bigrams.count(bigrams)/len(all_bigrams))
+    table_prob['probability'].append([f"{bigrams}: {probability}"])
 
 df = pd.DataFrame(table_prob)
 df.to_excel('probabilities.xlsx')
