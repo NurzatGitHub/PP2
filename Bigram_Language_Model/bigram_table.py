c9cba5210e8bac39256ae670c68b93a47c2903b0
NurzatGitHub <122099167+NurzatGitHub@users.noreply.github.com>
2023-05-05 12:06:31 +0600

    bigram

--- Bigram_Language_Model/bigram_table.py
+++ Bigram_Language_Model/bigram_table.py
@@ -1,6 +1,5 @@
-import torch
 import pandas as pd
-names = open("text.txt","r")
+names = open("names.txt","r")
 bigram_table = {'name':[],'bigram':[],'bigram_alternate_meeting':[]}
 all_bigrams = []
 
@@ -20,11 +19,6 @@ for name in names:
 
     bigram_table['bigram_alternate_meeting'].append(s_freq)
     bigram_table['bigram'].append(s)
-
-# info = ""
-# for bigrams in all_bigrams:
-#     info = f"{bigrams}: {all_bigrams.count(bigrams)}"
-#     bigram_table['all_bigrams'].append(info)
 
 df = pd.DataFrame(bigram_table)
 df.to_excel('bigram_table.xlsx')
--- Bigram_Language_Model/calculating_probabilities.py
+++ Bigram_Language_Model/calculating_probabilities.py
@@ -1,10 +1,12 @@
 import pandas as pd
-
 from bigram_table import all_bigrams
-table_prob = {'quantity':[]}
-
-for bigrams in all_bigrams:    
+set_bigrams = set(all_bigrams)
+table_prob = {'quantity':[],'probability':[]}
+probability = 0
+for bigrams in set_bigrams:
     table_prob['quantity'].append(f"{bigrams}: {all_bigrams.count(bigrams)}")
+    probability = all_bigrams.count(bigrams)/len(all_bigrams)
+    table_prob['probability'].append([probability])
 
 df = pd.DataFrame(table_prob)
-df.to_excel('calculating_probabilities.xlsx')
+df.to_excel('probabilities.xlsx')
